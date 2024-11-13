c96f778986857e61851de17269ac0e903c9a4cae
Anixer <anixer42@gmail.com>
2024-11-14 02:12:02 +0800

    Update gogo.js

--- src/gogo.js
+++ src/gogo.js
@@ -7,7 +7,7 @@ import {
   } from "./gogo_extractor.js";
  import * as cheerio from "cheerio";
 
-  const BaseURL = ["https://anitaku.so", "https://gogoanime3.co" , "https://gogoanimes.com.in/","https://gogoanime.co.ba/"]; 
+  const BaseURL = const BaseURL = ["https://gogoanimes.org.za", "https://gogoanime3.co" , "https://gogoanimes.com.in/","https://gogoanime.co.ba/"]; 
   const USER_AGENT =
     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.71 Safari/537.36";
   
